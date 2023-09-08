842e1b98024c2973eccc68a80f11f88934869e13
truongbien2501 <85795627+truongbien2501@users.noreply.github.com>
2023-09-08 09:43:49 +0700

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ class FTPApp(App):
         # Thông tin máy chủ FTP và đường dẫn đến file
         ftp_host = '113.160.225.111'
         ftp_user = 'kttvttbdb'
-        ftp_password = '618779'
+        ftp_password = '618778'
         file_path = '/Dulieu-Bantinkttvttb/5-Quang Ngai/PHAN MEM/DIENBAO'
 
         # Kết nối đến máy chủ FTP
